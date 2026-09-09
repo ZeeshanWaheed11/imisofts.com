afaf7a6ad13ab43c4e08c450eb324fc9acfb7a5b
Zeeshan Waheed <zeeshan@imisofts.com>
2026-09-09 17:41:41 +0500

    ops: allow Close and monday.com affiliate URLs in AFF allowlists

--- ops/fix_disclosures.py
+++ ops/fix_disclosures.py
@@ -2,7 +2,7 @@ import os, re, sys, glob
 ROOT = sys.argv[1] if len(sys.argv)>1 else '.'
 AFF = ['get.apollo.io/u5ocuv7me9t2','refer.instantly.ai/oovph6ghwnaa',
        'gohighlevel.com/?fp_ref=zeeshanwaheed','apify.com?fpr=rc63q','appsumo.8odi',
-       'get.brightdata.com/gf73pymzr2rh','try.leadpages.com/ax3hg5o6zyp9','partners.kit.com/080zlzt6wbb0']
+       'get.brightdata.com/gf73pymzr2rh','try.leadpages.com/ax3hg5o6zyp9','partners.kit.com/080zlzt6wbb0','refer.close.com/nkoci03o4zz4-fetl0j','try.monday.com/sy4a049vhjoj']
 DISC_RE = re.compile(r'<p class="affiliate-disclosure"[^>]*>.*?</p>\s*', re.S)
 def cta(url, label):
     return (f'<p class="aff-cta" style="margin:0 0 22px;"><a href="{url}" target="_blank" '
